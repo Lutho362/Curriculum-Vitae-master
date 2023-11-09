bdebb45b145df6be6861a979d6eb080e1937fdbb
Lutho362 <lutho470@gmail.com>
2023-11-09 11:20:06 +0200

    My Online Cv portfolio

---
+++ script.js
@@ -0,0 +1,68 @@
+var user = document .getElementById('user');
+var menu = document .getElementById('menu');
+var nav1 = document.getElementById("nav1");
+var nav2 = document.getElementById("nav2");
+var nav3 = document.getElementById("nav3");
+var nav4 = document.getElementById("nav4");
+var nav5 = document.getElementById("nav5");
+var smallNav = document.getElementById('small_nav');
+var back = document.getElementById('back')
+
+
+// menu.onclick = function(){
+//   if (user.classList == ""){
+//     user.classList.add("open");
+//   }else{
+//     user.classList.remove("open");
+//   }
+// }
+
+menu.addEventListener('click', ()=>{
+  if (smallNav.classList == 'close'){
+    smallNav.classList.remove('close');
+    smallNav.classList.add('open');
+  }else{
+    smallNav.classList.remove('open');  
+    smallNav.classList.add('close');
+  }
+  
+})
+
+back.addEventListener('click', ()=>{
+  smallNav.classList.remove('open');
+  smallNav.classList.add('close');
+})
+
+
+
+nav1.onclick = function(){
+  smallNav.classList.remove('open');
+  smallNav.classList.add('close');
+}
+nav2.onclick = function(){
+  smallNav.classList.remove('open');
+  smallNav.classList.add('close');
+}
+nav3.onclick = function(){
+  smallNav.classList.remove('open');
+  smallNav.classList.add('close');
+}
+nav4.onclick = function(){
+smallNav.classList.remove('open');
+smallNav.classList.add('close');
+}
+nav5.onclick = function(){
+  smallNav.classList.remove('open');
+  smallNav.classList.add('close');
+}
+
+
+
+
+
+
+
+
+
+
+
